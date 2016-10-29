0b1839d82ecc3aacd56e9ac232f5dcdf1568d394
winterwalk <jesterfourzero@gmail.com>
2016-10-29 17:34:13 -0400

    It works now.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #Automation script setup for RPi:
 #Requires 'pwnie express' libraries.
 #'''
+
 import subprocess
 from subprocess import call
 import os
@@ -10,11 +11,10 @@ import os
 print("Select a task to perform:\n")
 print("Task | Description\n")
 
-task = raw_input()
+task = input()
 script1 = "bash task1.sh"
 script2 = "bash task2.sh"
 script3 = "bash task3.sh"
-p
 if task == 'task1':
    os.system(script1)
 elif task == 'task2':
