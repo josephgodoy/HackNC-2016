6af00df151bdf436552b2fe68d22e2331a781258
winterwalk <jesterfourzero@gmail.com>
2016-10-29 15:23:20 -0400

    Fixed script-caller.

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-'''
-HackNC-2016: main.py
-Automation script setup for RPi:
-Requires 'pwnie express' libraries.
-'''
+#'''
+#HackNC 2016
+#Automation script setup for RPi:
+#Requires 'pwnie express' libraries.
+#'''
 import subprocess
+from subprocess import call
+import os
 
 print("Select a task to perform:\n")
 print("Task | Description\n")
 
-task = input("> ")
+task = raw_input()
+script1 = "bash task1.sh"
+script2 = "bash task2.sh"
+script3 = "bash task3.sh"
 
 if task == 'task1':
-   subprocess.call("./task1.sh", shell = True)
+   os.system(script1)
 elif task == 'task2':
-   subprocess.call("./task1.sh", shell = True)
+   os.system(script2)
 elif task == 'task3':
-   subprocess.call("./task3.sh", shell = True)
+   os.system(script3)
 
 # Rinse and repeat to add more tasks.
 #NOTE: Make sure to keep the bash scripts in the same directory.
